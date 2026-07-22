aea63f208f24de7f7588a81812d2d75ecab68203
EddyMamba <eddymamba98@gmail.com>
2026-07-22 09:16:49 +0100

    refactor(dashboard): restructure dashboard page with (overview) route group

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,18 +1,19 @@
 import { Card } from '@/app/ui/dashboard/cards';
+
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
+import { Suspense } from 'react';
 import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData, // 1. On rajoute l'importation de la fonction ici
-} from '@/app/lib/data';
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+   CardsSkeleton 
+} from '@/app/ui/skeletons';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  
-  // 2. On récupère et déstructure les 4 variables renvoyées par fetchCardData()
+  // Remove `const latestInvoices = await fetchLatestInvoices()`
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -26,7 +27,6 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* 3. On décommenté les cartes et on leur passe les bonnes valeurs */}
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -37,8 +37,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+         <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
     </main>
   );
